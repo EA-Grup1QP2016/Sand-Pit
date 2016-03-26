60ff12dfb267dd12e66c36cbd6543837516f8b95
Oriol Castaño <oriolcastanyo@gmail.com>
2016-03-26 21:02:06 +0100

    littlec

--- routes/database/database.js
+++ routes/database/database.js
@@ -32,20 +32,11 @@ function createUserDB(data, callback) {
                     console.log(LOG_TAG, error);
                     callback(false, error);
                 } else {
-<<<<<<< HEAD
                     listUsersDB(function(state, details){
                         console.log(LOG_TAG, "User saved in database");
                         callback(true, details);
                     });
-=======
-                    console.log(LOG_TAG, "User saved in database");
-<<<<<<< HEAD
-                    // Obtiene y devuelve todos los users tras crear uno
-                    callback (true)
-=======
                     callback(true)
->>>>>>> origin/master
->>>>>>> a56eb3593c39eeabcb8229eb74862f65eb9d9595
                 }
             });
         }
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,7 @@
-<<<<<<< HEAD
 var userCtrl = require("./user/user.js");
 var sandpitCtrl = require("./sandpit/sandpit.js");
-=======
-var user = require("./user/user.js");
-var sandpit = require("./sandpit/sandpit.js");
-<<<<<<< HEAD
-var event = require("./events/events.js");
+var eventCtrl = require("./events/events.js");
 
-module.exports = function(app) {
-    /**
-     * Generic Pages
-     */
-    
-    //User request
-    app.post("/createUser", user.createUser);
-    app.post("/loginUser", user.loginUser);
-    app.get("/listUsers", user.listUsers);
-    app.post("/removeUser", user.removeUser);
-    app.post("/updateUser", user.updateUser);
-=======
-var event = require("./event/event.js");
->>>>>>> origin/master
 
 module.exports = function(app) {
 
@@ -36,25 +17,18 @@ module.exports = function(app) {
     // Borrar un User
     //app.delete('/user/:user_id', userCtrl.removeUser);
 
->>>>>>> a56eb3593c39eeabcb8229eb74862f65eb9d9595
-
     //Sandpit request
-<<<<<<< HEAD
     //app.get("/listSandpits", sandpit.listSandpits);
     //app.post("/removeSandpit", sandpit.removeSandpit);
+
+    //Events request
+    //app.get("/listEvents", event.listEvents);
+    //app.post("/createEvent", event.createEvent);
+    //app.post("/removeEvent", event.removeEvent);
+
 
     // application -------------------------------------------------------------
     app.get('*', function(req, res) {
         res.sendfile('./public/admin.html'); // Carga única de la vista
     });
 };
-=======
-    app.get("/listSandpits", sandpit.listSandpits);
-    app.post("/removeSandpit", sandpit.removeSandpit);
-    
-    //Events request
-    app.get("/listEvents", event.listEvents);
-    app.post("/createEvent", event.createEvent);
-    app.post("/removeEvent", event.removeEvent);
-}
->>>>>>> origin/master
