c624e8b31f6ba4270494e1e7b60076eab8010b7d
Alejandro Sánchez <alejandro.sanchez.cabana@gmail.com>
2016-04-22 13:33:36 +0200

    Cors added

--- app.js
+++ app.js
@@ -14,14 +14,20 @@ var app = express();
 mongoose.connect('mongodb://localhost:27017/sandPit');
 
 app.use(logger('dev'));
-app.use(bodyParser.json({type: 'application/json'}));
+app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Cargamos los endpoints
 require('./routes/index.js')(app);
 
 app.listen(5000, function () {
-    console.log('Listening on port 5000...');
+  console.log('Listening on port 5000...');
 });
