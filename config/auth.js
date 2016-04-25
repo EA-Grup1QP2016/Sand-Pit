f924839fe2cd72e9bac701d208d5017e66a41288
piskitus <marcalarcon1994@gmail.com>
2016-04-25 11:31:48 +0200

    Oauth facebook funcionando
    Corregido el error de callback

--- config/auth.js
+++ config/auth.js
@@ -5,7 +5,7 @@ module.exports = {
     'facebookAuth': {
         clientID: '1583874288589589',
         clientSecret: '57849f4434453df3caa7709db80fdace',
-        callbackURL: 'http://localhost:5000/auth/facebook/callback',
+        callbackURL: 'http://localhost:5000/facebook/auth/facebook/callback',
         profileFields: ['id', 'displayName', 'photos', 'emails']// [1]
     }
 //'twitterAuth': { clientID: ..., clientSecret: ... }
