648a8a3a6537f21c702c1765751755b08f005e0e
piskitus <marcalarcon1994@gmail.com>
2016-04-25 11:57:03 +0200

    Agregación de iconos en login
    para implementación de twitter y google+ en breve

--- config/passport.js
+++ config/passport.js
@@ -42,22 +42,22 @@ module.exports = function(passport) {
 
 function myFacebookStrategy(token, refreshToken, profile, done) {
         // asynchronous
-        process.nextTick(function() {
+            process.nextTick(function() {
 //Save profile info into newUser object
-            var newUser = Object();
-            newUser.id = profile.id;
-            newUser.name = profile.displayName;
-            newUser.email = profile.emails[0].value; //multiple emails provided
-            newUser.pic = profile.photos[0].value; //url of the profile picture
-            newUser.provider = profile.provider;
+                var newUser = Object();
+                newUser.id = profile.id;
+                newUser.name = profile.displayName;
+                newUser.email = profile.emails[0].value; //multiple emails provided
+                newUser.pic = profile.photos[0].value; //url of the profile picture
+                newUser.provider = profile.provider;
 //Save the token for later actions with facebook (real actions
 //will require using facebook API or Node SDK (authorized by this token)
 //will require using facebook API or Node SDK (authorized by this token)
-            newUser.token = token;
+                newUser.token = token;
 //Assume the user is authenticated
 //newUser is made accessible through the session (req.user)
 //jump back to passport.authenticate()
-            return done(null, newUser);
+                return done(null, newUser);
         });
     }
 
