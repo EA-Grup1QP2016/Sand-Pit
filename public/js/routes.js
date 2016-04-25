466336a4a482ee585b41153d637709ac86ee05a9
Oriol Castaño <oriolcastanyo@gmail.com>
2016-04-25 23:12:01 +0200

    Mapa en view principal y gestión Parques

--- public/js/routes.js
+++ public/js/routes.js
@@ -9,14 +9,14 @@ angularRoutingApp.config(function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl : 'views/home.html',
-            controller  : 'mainController'
+            controller  : 'addCtrl'
+        })
+        .when('/parques', {
+            templateUrl : 'views/maps.html',
+            controller  : 'addCtrl'
         })
         .when('/eventos', {
             templateUrl : 'views/eventos.html',
-            controller  : 'eventosController'
-        })
-        .when('/parques', {
-            templateUrl : 'views/parques.html',
             controller  : 'parquesController'
         })
         .when('/registro', {
@@ -27,9 +27,13 @@ angularRoutingApp.config(function($routeProvider) {
             templateUrl : 'views/login.html',
             controller  : 'loginController'
         })
-        .when('/users', {
-            templateUrl : 'views/users1.html',
+        .when('/gestion-users', {
+            templateUrl : 'views/gestionUsuarios.html',
             controller  : 'usersController'
+        })
+        .when('/gestion-parques', {
+            templateUrl : 'views/gestionParques.html',
+            controller  : 'parksController'
         })
         .when('/maps', {
             templateUrl : 'views/maps.html',
@@ -41,7 +45,7 @@ angularRoutingApp.config(function($routeProvider) {
 });
 
 angularRoutingApp.controller('mainController', function($scope) {
-    $scope.message = 'Hola, Mundo!';
+    $scope.message = 'Hola, Mundooooo!';
 });
 
 angularRoutingApp.controller('addCtrl', function($scope, $http, $rootScope, geolocation, gservice){
@@ -281,5 +285,105 @@ angularRoutingApp.controller('usersController', function($scope, $http) {
     };
 });
 
+angularRoutingApp.controller('parksController', function($scope, $http) {
+    $scope.message = 'View de users';
+    $scope.newUser = {}; //Limpiamos formulario de registro
+    $scope.sandpits = {}; //Limpiamos tabla de usuarios
+    $scope.selected = false;
+    $scope.header = "Crear usuario"; //Mostramos "Crear usuario" en el panel derecho
+    $scope.update = true; //Escondemos el botón update
+    $scope.create = false; //Mostramos botñon create
+    $scope.error = false;
+    $scope.incomplete = false;
+    $scope.edit = true;
+
+    // Obtenemos todos los datos de la base de datos
+    $http.get('/sandpit').success(function(data) {
+            $scope.sandpits = data;
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // Función para registrar un user
+    $scope.createUser = function() {
+        $http.post('/user', $scope.newUser)
+            .success(function(data) {
+                $scope.newUser = {}; // Borramos los datos del formulario
+                $scope.users = data;
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // Función que borra un objeto user conocido su id
+    $scope.deleteUser = function(id) {
+        $http.delete('/user/' + id)
+            .success(function(data) {
+                $scope.newUSER = {};
+                $scope.users = data;
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+
+    // Función para coger el objeto seleccionado en la tabla antes de editarlo
+    $scope.selectUser = function(user) {
+        $scope.newUser = user;
+        $scope.selected = true;
+        $scope.header = "Editar usuario";
+        $scope.update = false;
+        $scope.create = true;
+
+        console.log($scope.newUser, $scope.selected);
+    };
+
+    // Función para editar los datos de una persona
+    $scope.editUser = function() {
+        $http.put('/user/' + $scope.newUser._id, $scope.newUser)
+            .success(function(data) {
+                $scope.newUser = {}; // Borramos los datos del formulario
+                $scope.users = data;
+                $scope.selected = false;
+                $scope.header = "Crear usuario";
+                $scope.update = true;
+                $scope.create = false;
+            })
+            .error(function(data) {
+                $scope.header = "Crear usuario";
+                $scope.update = true;
+                $scope.create = false;
+                console.log('Error: ' + data);
+            });
+    };
+
+    $scope.edit = true;
+    $scope.error = false;
+    $scope.incomplete = false;
+    $scope.hideform = true;
+
+    $scope.$watch('newUser.password',function() {$scope.test();});
+    $scope.$watch('pass2',function() {$scope.test();});
+    $scope.$watch('fullName', function() {$scope.test();});
+
+
+
+    $scope.test = function() {
+        if ($scope.newUser.password !== $scope.pass2) {
+            $scope.error = true;
+        } else {
+            $scope.error = false;
+        }
+        $scope.incomplete = false;
+        if ($scope.edit && (!$scope.fullName.length ||
+            !$scope.lName.length ||
+            !$scope.newUser.password.length || !$scope.pass2.length)) {
+            $scope.incomplete = true;
+        }
+    };
+});
 
 
